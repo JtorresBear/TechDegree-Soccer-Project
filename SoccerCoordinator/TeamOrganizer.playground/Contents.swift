238d438b0b7755e1c7ec9ac843cb6537c6d06590
Juan Torres <jmmtorres1991@yahoo.com>
2018-06-23 02:09:52 -0500

    Added the applicants for soccer

--- SoccerCoordinator/TeamOrganizer.playground/Contents.swift
+++ SoccerCoordinator/TeamOrganizer.playground/Contents.swift
@@ -1,5 +1,28 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+// manually created an empty array
+var players: [[String: Any]] = []
 
-var str = "Hello, playground"
+players.append(["Name": "Joe Smith", "Height": 42, "Soccer Experience": true, "Guardians": "Jim and Jan Smith"])
+players.append(["Name": "Jill Tanner", "Height": 36, "Soccer Experience": true, "Guardians": "Clara Tanner"])
+players.append(["Name": "Bill Bon", "Height": 43, "Soccer Experience": true , "Guardians": "Sara and Jenny Bon"])
+players.append(["Name": "Eva Gordon", "Height": 45, "Soccer Experience": false, "Guardians": "Wendy and Mike Gordon"])
+players.append(["Name": "Matt Gill", "Height": 40, "Soccer Experience": false, "Guardians": "Charles and Sylvia Gill"])
+players.append(["Name": "Kimmy Stein", "Height": 41, "Soccer Experience": false, "Guardians": "Bill and Hillary Stein"])
+players.append(["Name": "Sammy Adams", "Height": 45, "Soccer Experience": false, "Guardians": "Jeff Adams"])
+players.append(["Name": "Karl Saygan", "Height": 42, "Soccer Experience": true , "Guardians": "Heather Bledsoe"])
+players.append(["Name": "Suzane Greenberg", "Height": 44, "Soccer Experience": true, "Guardians": "Henrietta Dumas"])
+players.append(["Name": "Sal Dali", "Height": 41, "Soccer Experience": false , "Guardians": "Gala Dali"])
+players.append(["Name": "Joe Kavalier", "Height": 39 , "Soccer Experience": false, "Guardians": "Sam and Elaine Kavalier"])
+players.append(["Name": "Ben Finkelstein", "Height": 44, "Soccer Experience": false, "Guardians": "Aaron and Jill Finkelstein"])
+players.append(["Name": "Diego Soto", "Height": 41, "Soccer Experience": true, "Guardians": "Robin and Sarika Soto"])
+players.append(["Name": "Chloe Alaska", "Height": 47, "Soccer Experience": false , "Guardians": "David and Jamie Alaska"])
+players.append(["Name": "Arnold Willis", "Height": 43, "Soccer Experience": false, "Guardians": "Claire Willis"])
+players.append(["Name": "Phillip Helm", "Height": 44, "Soccer Experience": true, "Guardians": "Thomas Helm and Eva Jones"])
+players.append(["Name": "Les Clay", "Height": 42, "Soccer Experience": true , "Guardians": "Wynonna Brown"])
+players.append(["Name": "Herschel Krustofski", "Height": 45, "Soccer Experience": true, "Guardians": "Hyman and Rachel Krustofski"])
+
+//var num = players.count
+
+//print(num)
